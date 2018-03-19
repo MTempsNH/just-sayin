f81a17859bda65369dc68b7b6f25d81f6cc6122c
Mark Temple <mtemps@gmail.com>
2018-03-19 14:00:00 -0400

    Adding Hero component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Hero from './components/Hero';
+
 class App extends Component {
   render() {
     return (
-    <div class="wrapper">
-        <header class="header">Header</header>
-        <article class="main">
-            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+    <div className="wrapper">
+        <header className="header">Header</header>
+        <article className="main">
+            <Hero />
         </article>
-        <footer class="footer">Footer</footer>
+        <footer className="footer">Footer</footer>
     </div>
     );
   }
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+    render() {
+        return (
+            <div className="hero">
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            </div>
+        );
+    }
+}
+
+export default Hero;
