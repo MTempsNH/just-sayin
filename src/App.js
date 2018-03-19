a3eeaa7f7d79aee0bec4c6ea2e65e70009025225
Mark Temple <mtemps@gmail.com>
2018-03-19 13:28:53 -0400

    First code push. Base application

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div class="wrapper">
+        <header class="header">Header</header>
+        <article class="main">
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        </article>
+        <footer class="footer">Footer</footer>
+    </div>
+    );
+  }
+}
+
+export default App;
